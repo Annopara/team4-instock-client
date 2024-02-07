b08d5fd4e392e9fdf77c911d926ee87c3a1e7f06
mohamasia <mohamedasia1@gmail.com>
2024-02-07 22:36:02 +0100

    Updated footer

--- src/components/Footer/.ipynb_checkpoints/Footer-checkpoint.js
+++
@@ -1,8 +0,0 @@
-import "./Footer.scss"
-export const Footer = () => {
-    return (
-        <footer className="footer">
-        <p className="footer__text">&copy; InStock Inc. All Rights Reserved.</p>
-        </footer>
-    );
-    };
