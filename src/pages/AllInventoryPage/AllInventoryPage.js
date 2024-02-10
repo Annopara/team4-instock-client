6d018d4bd8fd6fc7c755b8c2fa38160bf2a8e6e0
libbieiscool <whoislibbiefarrell@gmail.com>
2024-02-09 19:01:26 -0700

    deleting one or two lines

--- src/pages/AllInventoryPage/AllInventoryPage.js
+++ src/pages/AllInventoryPage/AllInventoryPage.js
@@ -2,7 +2,6 @@ import { SearchHeader } from '../../components/SearchHeader/SearchHeader';
 import './AllInventoryPage.scss';
 // remember to import as {} since it's not the default export
 import {InventoryHeader} from '../../components/InventoryHeader/InventoryHeader'
-import InventoryList from '../../components/InventoryList/InventoryList';
 
 export const AllInventoryPage = () => {
   return (
